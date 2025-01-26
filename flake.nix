d68c3b780808facb689facefa8398487e07d1a6a
Jon-S-Wick <jwick9137@gmail.com>
2025-01-26 14:34:25 -0800

    Back to normal conf, Bundle and home manager module does not work

--- flake.nix
+++ flake.nix
@@ -12,12 +12,24 @@
       "github:nix-community/home-manager"; # Home Manager flake input
 
   };
-  outputs = { self, nixvim, flake-parts, nixpkgs, ... }@inputs:
+#   outputs = { self, nixvim, flake-parts, nixpkgs, ... }@inputs:
+#     flake-parts.lib.mkFlake { inherit inputs; } {
+#       package = { };
+#       homeManagerModules = { };
+#
+#       systems =
+#         [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+#
+#     };
+# }
+
+  outputs = { nixvim, flake-parts, nixpkgs, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
+
+
 
       systems =
         [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-
       perSystem = { system, pkgs, ... }:
         let
           nixvimLib = nixvim.lib.${system};
@@ -41,83 +53,9 @@
 
           packages = {
             # Lets you run `nix run .` to start nixvim
-            default = nvim;
+             default = nvim;
           };
+
         };
     };
 }
-#   outputs = { nixvim, flake-parts, nixpkgs, ... }@inputs:
-#     flake-parts.lib.mkFlake { inherit inputs; } {
-#
-#       # homeManagerModules.nixvim = { system, pkgs, ... }:
-#       #
-#       #   let
-#       #     nixvimLib = nixvim.lib.${system};
-#       #     nixvim' = nixvim.legacyPackages.${system};
-#       #     nixvimModule = {
-#       #       inherit pkgs;
-#       #       inherit system; # or alternatively, set `pkgs`
-#       #       module = import ./config; # import the module directly
-#       #       # You can use `extraSpecialArgs` to pass additional arguments to your module files
-#       #       extraSpecialArgs = {
-#       #         # inherit (inputs) foo;
-#       #       };
-#       #     };
-#       #     nvim = nixvim'.makeNixvimWithModule nixvimModule;
-#       #   in { imports = [ ./config/default.nix ]; };
-#       # systems =
-#       #   [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-#       system = "x86_64-linux";
-#       # homeManagerModules.nixvim = { system, pkgs, home-manager, ... }: {
-#       #   programs.neovim.enable = true;
-#       # };
-#
-#       # let
-#       #   nixvimLib = nixvim.lib.${system};
-#       #   nixvim' = nixvim.legacyPackages.${system};
-#       #   nixvimModule = {
-#       #     inherit pkgs;
-#       #     inherit system; # or alternatively, set `pkgs`
-#       #     module = import ./config; # import the module directly
-#       #     # You can use `extraSpecialArgs` to pass additional arguments to your module files
-#       #     extraSpecialArgs = {
-#       #       # inherit (inputs) foo;
-#       #     };
-#       #   };
-#       #   nvim = nixvim'.makeNixvimWithModule nixvimModule;
-#       # in { pkgs, ... }@args: {
-#       #   imports = [
-#       #     (import ./config/default.nix inputs {
-#       #       homeManagerModule = self.homeManagerModules;
-#       #     })
-#       #   ];
-#       # };
-#
-#       perSystem = { system, pkgs, ... }:
-#         let
-#           nixvimLib = nixvim.lib.${system};
-#           nixvim' = nixvim.legacyPackages.${system};
-#           nixvimModule = {
-#             inherit pkgs;
-#             inherit system; # or alternatively, set `pkgs`
-#             module = import ./config; # import the module directly
-#             # You can use `extraSpecialArgs` to pass additional arguments to your module files
-#             extraSpecialArgs = {
-#               # inherit (inputs) foo;
-#             };
-#           };
-#           nvim = nixvim'.makeNixvimWithModule nixvimModule;
-#         in {
-#           checks = {
-#             # Run `nix flake check .` to verify that your config is not broken
-#             default =
-#               nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
-#           };
-#
-#           packages = {
-#             # Lets you run `nix run .` to start nixvim
-#             default = nvim;
-#           };
-#         };
-#     };
-# }
